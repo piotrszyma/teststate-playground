e39f8e8931428493fababb3c3357aa435d04d653
Piotr Szyma <pszyma@opera.com>
2022-05-04 20:07:16 +0200

    feat: test

--- main.spec.ts
+++ main.spec.ts
@@ -1,6 +1,27 @@
 import { add } from "./main";
-import { it, expect } from "@jest/globals";
+import { it, expect, beforeEach } from "@jest/globals";
+import { resetState } from "teststate";
 
 it("adds", () => {
   expect(add(2, 2)).toEqual(4);
+});
+
+const state = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+beforeEach(() => {
+  resetState(state);
+});
+
+it("clears state", () => {
+  expect(state.a).toEqual(1);
+  state.a = 2;
+});
+
+it("clears state", () => {
+  expect(state.a).toEqual(1);
+  state.a = 3;
 });
