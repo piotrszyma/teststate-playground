73503b70547042af0e9bd36aba5ccd269321a3c6
Piotr Szyma <pszyma@opera.com>
2022-05-09 17:44:02 +0200

    fix: nested state

--- main.spec.ts
+++ main.spec.ts
@@ -1,5 +1,5 @@
 import { add } from "./main";
-import { it, expect, beforeEach } from "@jest/globals";
+import { it, expect, beforeEach, describe } from "@jest/globals";
 import { resetState } from "teststate";
 
 it("adds", () => {
@@ -24,4 +24,48 @@ it("clears state", () => {
 it("clears state", () => {
   expect(state.a).toEqual(1);
   state.a = 3;
+});
+
+describe("a test", () => {
+  const nestedState = {
+    d: 1,
+    e: 2,
+    f: 3,
+  };
+
+  beforeEach(() => {
+    resetState(nestedState);
+  });
+
+  it("clears nested state", () => {
+    expect(nestedState.d).toEqual(1);
+    nestedState.d = 3;
+  });
+
+  it("clears nested state", () => {
+    expect(nestedState.d).toEqual(1);
+    nestedState.d = 3;
+  });
+
+  describe("nested test", () => {
+    const nestedNestedState = {
+      g: 7,
+      h: 8,
+      i: 9,
+    };
+
+    beforeEach(() => {
+      resetState(nestedNestedState);
+    });
+
+    it("clears 2x nested state", () => {
+      expect(nestedNestedState.g).toEqual(7);
+      nestedNestedState.g = 1;
+    });
+
+    it("clears 2x nested state", () => {
+      expect(nestedNestedState.g).toEqual(7);
+      nestedNestedState.g = 2;
+    });
+  });
 });
